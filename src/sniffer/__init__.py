2db20251940bd566042d2cfd560765718fc16956
Mark Servilla <mark.servilla@gmail.com>
2020-11-27 10:59:22 -0700

    Add __init__ dependencies for setup.py

---
+++ src/sniffer/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+:Mod: __init__
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    11/27/20
+"""
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
